a04e895fc1be28c5eea127a2ce66a15d0cea5b84
thealexlittle <alexlittle2college@gmail.com>
2019-12-18 10:07:42 -0800

    Rudimentery App

--- App.js
+++ App.js
@@ -2,25 +2,42 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createTopTabNavigator} from 'react-navigation-tabs';
 import LoginScreen from './src/screens/Auth/LoginScreen';
 import RegisterScreen from './src/screens/Auth/RegisterScreen';
+
 import HomeScreen from './src/screens/App/HomeScreen';
+import StreakScreen from './src/screens/App/StreakScreen';
 
 //Add Screens As they are created here
-const AppStack = createStackNavigator(
+const AppStack = createTopTabNavigator(
   {
+    Streaks: StreakScreen,
     Home: HomeScreen,
     // ex. Friends : FriendScreen,
   },
   {
     initialRouteName: 'Home', //Can change this during development
+    tabBarPosition: 'bottom',
+  },
+  {
+    defaultNavigationOptions: {
+      header: null,
+    },
   },
 );
 
-const AuthStack = createStackNavigator({
-  Login: LoginScreen,
-  Register: RegisterScreen,
-});
+const AuthStack = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Register: RegisterScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      header: null,
+    },
+  },
+);
 
 //Navigation Flow for the app
 //Initial Route is set to App for development purposes
@@ -31,7 +48,7 @@ export default createAppContainer(
       Auth: AuthStack,
     },
     {
-      initialRouteName: 'Auth',
+      initialRouteName: 'App',
     },
   ),
 );
