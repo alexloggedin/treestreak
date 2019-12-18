bd2d36627c8b80591666722dfcbd53670eb0b4f4
apandit42 <apandit@stanford.edu>
2019-12-17 19:13:28 -0800

    0.0.2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 class TreeStreak extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View>
         <Text>Hello, world!</Text>
       </View>
     );
